eed0407d0705b81bbee4a738333498f74fa9afae
lgbadluck <lg.badluck@gmail.com>
2025-03-17 08:15:42 +0200

    Added ExceptionAdvice and ErrorResponse dto

---
+++ src/main/java/com/softuni/notification_svc/web/dto/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.softuni.notification_svc.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private LocalDateTime time;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.time = LocalDateTime.now();
+    }
+}
